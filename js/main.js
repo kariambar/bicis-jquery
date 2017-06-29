15a74753aa87700e15c9644006ba39c1d77b08aa
karina palacios <karina.spalacios@gmail.com>
2017-06-28 22:21:22 -0400

    validar nombre

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-function validateForm(){
+
 	/* Escribe tú código aquí */
-}
+	$("#name").on("blur", function() {
+    if ( $(this).val().match('^[a-zA-Z]$') ) {
+    } else {
+        alert("Ingresa un numero valido");
+    }
+});
